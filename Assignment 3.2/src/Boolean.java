606bf759e76979c47f8ed5530f529831821033f0
Donrockz <rockstarrockey18@gmail.com>
2017-06-07 05:28:20 +0530

    Add files via upload

---
+++ Assignment 3.2/src/Boolean.java
@@ -0,0 +1,18 @@
+
+public class Boolean {
+
+	public static void main(String[] args) {
+
+		String S1 = "value";
+		String S2 = "value";
+
+		if (S1 == S2) {
+			boolean same = true;
+			System.out.println(same);
+		} else {
+			boolean same = false;
+			System.out.println(same);
+		}
+
+	}
+}
